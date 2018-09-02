b0b841f67c67025f01596315e1ec6b97210371af
Nick Attel <nickattel@gmail.com>
2018-09-02 01:38:52 -0700

    working thumbnails/search... links/buttons not working

--- think-tube.js
+++ think-tube.js
@@ -4,15 +4,20 @@ function getDataFromApi(searchTerm, callback) {
   const query = {
     part: 'snippet',
     key: 'AIzaSyDr0_4MezaO99bgCfnzCo6VN5aWmxtFL2U',
-    q: `${searchTerm}`,
-    maxResults: 5
+    q: searchTerm,
+    maxResults: 5,
+    pageToken: ''
   }
   $.getJSON(endpointUrl, query, callback);
 }
 
 function renderResult(result) {
-  return 
-  `<div>${result.part}</div>`
+  const resultLayout = `<div class='videoLink'><img class='thumbnail' src=""></div><div class='channelLink'></div>`;
+  const layout = $(resultLayout);
+  layout.find('.thumbnail').attr('src', result.snippet.thumbnails.medium.url);
+  layout.find('.videoLink').attr('href', result.id.videoId);
+  layout.find('.channelLink').attr('href', result.snippet.channelId);
+  return layout;
 }
 
 function displayData(data) {
